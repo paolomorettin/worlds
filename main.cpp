1d843a99bc66291c0bbebd6f259c607c9d87b51e
paolomorettin <morettin.paolo@gmail.com>
2015-05-08 17:58:23 +0100

    kinda makes sense

--- main.cpp
+++ main.cpp
@@ -25,30 +25,32 @@ bool createLevel(scene::ISceneManager * smgr,scene::ISceneNode * parent,scene::I
 
   scene::IMetaTriangleSelector * metaselector = smgr -> createMetaTriangleSelector();
 
-  int scale = 1;
-  core::vector3d<int> level_size = core::vector3d<int>(10,10,10);
-  W_LevelGenerator level = W_LevelGenerator(level_size);
+  int scale = 150;
+  core::vector3d<int> level_size = core::vector3d<int>(50,100,50);
+  W_LevelGenerator level = W_LevelGenerator(level_size,1000);
+
   core::list<W_Structure> * structures = level.getStructures();
 
+
+
+  // create the structures
   core::list<W_Structure>::Iterator iterator;
   for (iterator = structures -> begin(); iterator != structures -> end(); iterator++) {
-
     
     W_Structure current = *iterator;
     core::vector3df position = core::vector3df(current.pos_x, current.pos_y, current.pos_z);
     core::vector3df size = core::vector3df(current.size_x, current.size_y, current.size_z);
 
-    scene::IMeshSceneNode* map_node = smgr -> addCubeSceneNode(2,parent,IDFlag_IsPickable);
-    map_node -> setScale(scale * size);
-    map_node -> setPosition(scale/2 *size);
-    map_node -> setPosition(scale * position);
+    scene::IMeshSceneNode* map_node = smgr -> addCubeSceneNode(1,parent,IDFlag_IsPickable);
 
-    
+    map_node -> setPosition((scale/2 *size) + scale * position);
+    map_node -> setScale(scale * size);
+
     switch (rand() % 3) {
     case 0 :
     map_node -> getMaterial(0).AmbientColor = video::SColor(255,255,0,0);
     map_node -> getMaterial(0).DiffuseColor = video::SColor(100,100,100,255);
- map_node -> setMaterialTexture( 0, smgr -> getVideoDriver() -> getTexture("./media/blue.png") );
+    map_node -> setMaterialTexture( 0, smgr -> getVideoDriver() -> getTexture("./media/blue.png") );
     break;
     case 1 :
       map_node -> getMaterial(0).AmbientColor = video::SColor(255,0,255,0);
@@ -58,7 +60,7 @@ bool createLevel(scene::ISceneManager * smgr,scene::ISceneNode * parent,scene::I
     case 2 :
     map_node -> getMaterial(0).AmbientColor = video::SColor(255,0,0,255);
     map_node -> getMaterial(0).DiffuseColor = video::SColor(100,100,255,100);
- map_node -> setMaterialTexture( 0, smgr -> getVideoDriver() -> getTexture("./media/green.png") );
+    map_node -> setMaterialTexture( 0, smgr -> getVideoDriver() -> getTexture("./media/green.png") );
     break;
 
 
@@ -71,20 +73,31 @@ bool createLevel(scene::ISceneManager * smgr,scene::ISceneNode * parent,scene::I
     metaselector -> addTriangleSelector(selector);
     selector->drop();
 
-
-
   }
-  
-  //scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(metaselector, camera, core::vector3df(30,50,30), core::vector3df(0,-10,0), core::vector3df(0,30,0));
 
-  //camera->addAnimator(anim);
+  // create start/end point
+  vector3d<int> startcell = level.getStart();
+  vector3df sp = vector3df(startcell.X, startcell.Y, startcell.Z);
+  /*
+  scene::IMeshSceneNode * start_node = smgr -> addSphereSceneNode(2*scale);
+  start_node -> setID(ID_IsNotPickable);
+  start_node -> setPosition(scale * sp);*/
+  camera -> setPosition(scale * sp);
+  printf("Starting cell: (%i,%i,%i)\n",startcell.X,startcell.Y,startcell.Z);
+  vector3df camerapos = camera -> getPosition();
+  printf("Starting point: (%f,%f,%f)\n",camerapos.X,camerapos.Y,camerapos.Z);
+
+  vector3d<int> endcell = level.getEnd();
+  vector3df ep = vector3df(endcell.X, endcell.Y, endcell.Z);
+  scene::IMeshSceneNode * end_node = smgr -> addSphereSceneNode(2*scale);  
+  end_node -> setPosition(scale * ep);
+  
+  scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(metaselector, camera, core::vector3df(30,50,30), core::vector3df(0,-10,0), core::vector3df(0,30,0));
+
+  camera->addAnimator(anim);
   
   metaselector -> drop();
-  //anim->drop();  
-
-  /*
-  map_node -> setMaterialTexture( 0, smgr -> getVideoDriver() -> getTexture("./media/red.png") );
-  */
+  anim->drop();  
 
   return true;
   
@@ -139,17 +152,43 @@ int main()
   scene::ISceneManager* smgr = device->getSceneManager();
     
   // add the camera (FPS-like)
-  scene::ICameraSceneNode * camera = smgr -> addCameraSceneNodeFPS(0, 100.0f, .3f, ID_IsNotPickable, 0, 0, true, 3.f);
 
+  //scene::ICameraSceneNode * camera = smgr -> addCameraSceneNodeFPS(0, 100.0f, .3f, ID_IsNotPickable, 0, 0, true, 3.f);
+  SKeyMap keyMap[10];
+  keyMap[0].Action = EKA_MOVE_FORWARD;
+  keyMap[0].KeyCode = KEY_UP;
+  keyMap[1].Action = EKA_MOVE_FORWARD;
+  keyMap[1].KeyCode = KEY_KEY_W;
+  
+  keyMap[2].Action = EKA_MOVE_BACKWARD;
+  keyMap[2].KeyCode = KEY_DOWN;
+  keyMap[3].Action = EKA_MOVE_BACKWARD;
+  keyMap[3].KeyCode = KEY_KEY_S;
+  
+  keyMap[4].Action = EKA_STRAFE_LEFT;
+  keyMap[4].KeyCode = KEY_LEFT;
+  keyMap[5].Action = EKA_STRAFE_LEFT;
+  keyMap[5].KeyCode = KEY_KEY_A;
+  
+  keyMap[6].Action = EKA_STRAFE_RIGHT;
+  keyMap[6].KeyCode = KEY_RIGHT;
+  keyMap[7].Action = EKA_STRAFE_RIGHT;
+  keyMap[7].KeyCode = KEY_KEY_D;
 
+  keyMap[8].Action = EKA_CROUCH;
+  keyMap[8].KeyCode = KEY_SHIFT;
+
+  keyMap[9].Action = EKA_JUMP_UP;
+  keyMap[9].KeyCode = KEY_SPACE;
+  
+  
+  scene::ICameraSceneNode* camera = smgr -> addCameraSceneNodeFPS(0, 100, 0.3, -1, keyMap, 10, true, 10);
+  
   
   smgr -> setAmbientLight(video::SColor(100,1,1,50));
 
-  scene::ILightSceneNode* light1 = smgr -> addLightSceneNode( camera, core::vector3df(0,100,0), video::SColor(255,255,255,255), 10.0f, ID_IsNotPickable ); 
-  //scene::ILightSceneNode* light1 = smgr -> addLightSceneNode( 0, core::vector3df(0,-800,0), video::SColor(255,255,255,255), 30.0f, ID_IsNotPickable ); 
-  //camera -> setPosition(core::vector3df(0,1000,0));
-
-
+  scene::ILightSceneNode* light1 = smgr -> addLightSceneNode( camera, core::vector3df(0,0,0), video::SColor(255,255,255,255), 30.0f, ID_IsNotPickable ); 
+  scene::ILightSceneNode* light2 = smgr -> addLightSceneNode( 0, core::vector3df(0,-100,0), video::SColor(255,255,255,255), 30.0f, ID_IsNotPickable ); 
   
   if (!createLevel(smgr,0,camera))
     return false;
