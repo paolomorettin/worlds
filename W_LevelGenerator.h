4117bbebc370c93d745b183c2c42d21c4e2be78e
Daniele Campana <danicampa90@gmail.com>
2015-05-09 20:40:21 +0200

    Cleanup indentation & style in Level Generator

--- W_LevelGenerator.h
+++ W_LevelGenerator.h
@@ -12,17 +12,17 @@ using namespace core;
 
 class W_LevelGenerator {
  public:
-  W_LevelGenerator(vector3d<int>,int);
+	W_LevelGenerator(vector3d<int>,int);
 
-  list<W_Structure> * getStructures();
-  vector3d<int> getSize();
-  vector3d<int> getStart();
-  vector3d<int> getEnd();
+	list<W_Structure>* getStructures();
+	vector3d<int> getSize();
+	vector3d<int> getStart();
+	vector3d<int> getEnd();
 
  private:
-  list<W_Structure> structures;
-  vector3d<int> size;
-  vector3d<int> start;
-  vector3d<int> end;
+	list<W_Structure> structures;
+	vector3d<int> size;
+	vector3d<int> start;
+	vector3d<int> end;
 
 };
