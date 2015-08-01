de156cee83a3fcb5d4cacc0d15729e698feea1ba
manoet <manoet.ita@gmail.com>
2015-08-01 11:43:07 +0200

    Changed indentation in Player.hpp

--- src/Player.hpp
+++ src/Player.hpp
@@ -6,29 +6,29 @@
 class btRigidBody;
 
 class PlayerGameObj: public IGameObject {
-	scene::ICameraSceneNode *camera; // irr camera
-	scene::ILightSceneNode *playerlight; // irr light that follows the player.
-	btRigidBody *rigid_body;
+    scene::ICameraSceneNode *camera; // irr camera
+    scene::ILightSceneNode *playerlight; // irr light that follows the player.
+    btRigidBody *rigid_body;
 
 public:
 
-	explicit PlayerGameObj():
-		IGameObject("Player"), playerlight(nullptr), camera(nullptr), rigid_body(nullptr)
-	{
-	}
-	// virtual (override of IGameObject)
-	// called by bullet
-	virtual void setWorldTransform(const btTransform& centerOfMassWorldTrans)
-	{
-		IGameObject::setWorldTransform(centerOfMassWorldTrans);
-		btVector3 pos = m_graphicsWorldTrans.getOrigin();
-		camera->setPosition(core::vector3df(pos.x(), pos.y(), pos.z()));
-	}
+    explicit PlayerGameObj():
+        IGameObject("Player"), playerlight(nullptr), camera(nullptr), rigid_body(nullptr)
+    {
+    }
+    // virtual (override of IGameObject)
+    // called by bullet
+    virtual void setWorldTransform(const btTransform& centerOfMassWorldTrans)
+    {
+        IGameObject::setWorldTransform(centerOfMassWorldTrans);
+        btVector3 pos = m_graphicsWorldTrans.getOrigin();
+        camera->setPosition(core::vector3df(pos.x(), pos.y(), pos.z()));
+    }
 
-	virtual void render(GameLoop&, float);
-	virtual btRigidBody* initialize(GameLoop&, const vector3df& start_pos);
-	virtual void logic_tick(GameLoop&);
+    virtual void render(GameLoop&, float);
+    virtual btRigidBody* initialize(GameLoop&, const vector3df& start_pos);
+    virtual void logic_tick(GameLoop&);
 
-	virtual ~PlayerGameObj() {};
+    virtual ~PlayerGameObj() {};
 };
 
